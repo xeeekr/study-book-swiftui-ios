bbef00302721bb3cc23edc9141d6f4c0e47f1e8d
xeeekr <xeeekr@gmail.com>
2024-09-24 18:21:51 +0900

    📝 DAY-01

---
+++ ch04/LearnSwift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Welcome to Swift")
+/*:
+# Welcome to Playgrounds
+ This is your *first* playground which is intended to demonstrate:
+ * The use of **Quick Look**
+ * Placing results **in-line** with the code
+ */
+
+var x = 10
+
+for index in 1...20 {
+    let y = index * x
+    x -= 1
+    print(y)
+}
+
+//: 한 줄 주석
+
+/*:
+ 여러줄 주석
+ */
